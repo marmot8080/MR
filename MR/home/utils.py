1b76a46803807b4fa8fb127908903a5c58b0feb8
marmot8080 <marmot8080@gmail.com>
2024-09-09 17:48:09 +0900

    modified candidate count

--- MR/home/utils.py
+++ MR/home/utils.py
@@ -9,7 +9,7 @@ def get_recent_popular_movie_list():
 def get_movie_list_by_genre(genre: int):
     movie_list_by_genre = MovieInfo.objects.filter(genres__contains=[genre])
 
-    latest_200_movies = movie_list_by_genre.order_by('-release_date')[:100]
+    latest_200_movies = movie_list_by_genre.order_by('-release_date')[:200]
     top_20_popular_movies = latest_200_movies.order_by('-popularity')[:20]
 
     return top_20_popular_movies
