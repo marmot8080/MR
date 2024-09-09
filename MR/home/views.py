8b9a234d9ee4917bef431c7084932d29b5b6130e
marmot8080 <marmot8080@gmail.com>
2024-09-09 17:48:51 +0900

    added example data

--- MR/home/views.py
+++ MR/home/views.py
@@ -3,11 +3,63 @@ from .utils import *
 
 def home(request):
 
+    # movie_info = {
+    #     'recent_movies': get_recent_popular_movie_list(),
+    #     'horror_movies': get_movie_list_by_genre(),
+    #     'comedy_movies': get_movie_list_by_genre(),
+    #     'animation_movies': get_movie_list_by_genre(),
+    # }
+
+    # 예시 값
     movie_info = {
-        'recent_movies': get_recent_popular_movie_list(),
-        'horror_movies': get_movie_list_by_genre(),
-        'comedy_movies': get_movie_list_by_genre(),
-        'animation_movies': get_movie_list_by_genre(),
+        'recent_movies': [{'id': 1,
+                           'title': 'a',
+                          'image': '',
+                          'rating': 3},
+                          {'id': 2,
+                           'title': 'b',
+                          'image': '',
+                          'rating': 4},
+                          {'id': 3,
+                           'title': 'c',
+                          'image': '',
+                          'rating': 2},
+                          {'id': 4,
+                           'title': 'd',
+                          'image': '',
+                          'rating': 1},
+                          {'id': 1,
+                           'title': 'a',
+                          'image': '',
+                          'rating': 3},
+                          {'id': 2,
+                           'title': 'b',
+                          'image': '',
+                          'rating': 4},
+                          {'id': 3,
+                           'title': 'c',
+                          'image': '',
+                          'rating': 2},
+                          {'id': 4,
+                           'title': 'd',
+                          'image': '',
+                          'rating': 1},
+                          {'id': 1,
+                           'title': 'a',
+                          'image': '',
+                          'rating': 3},
+                          {'id': 2,
+                           'title': 'b',
+                          'image': '',
+                          'rating': 4},
+                          {'id': 3,
+                           'title': 'c',
+                          'image': '',
+                          'rating': 2},
+                          {'id': 4,
+                           'title': 'd',
+                          'image': '',
+                          'rating': 1},],
     }
 
-    return render(request, 'home.html', movie_info)
+    return render(request, 'home.html', {'movie_info': movie_info})
